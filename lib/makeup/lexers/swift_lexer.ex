4244072a17e93d79753a2dd5b9d523bfaacecd4c
Jesse Claven <jesse@duffel.com>
2023-07-18 17:07:04 +0100

    feat: Add literals, comments, whitespace

--- lib/makeup/lexers/swift_lexer.ex
+++ lib/makeup/lexers/swift_lexer.ex
@@ -1,7 +1,10 @@
 defmodule Makeup.Lexers.SwiftLexer do
   @moduledoc """
-  Lexer for the Swift language to be used
-  with the Makeup package.
+  Lexer for the Swift language to be used with the Makeup package.
+
+  Based on the official language reference [1].
+
+  [1] https://docs.swift.org/swift-book/documentation/the-swift-programming-language/aboutthelanguagereference
   """
 
   import NimbleParsec
@@ -16,7 +19,121 @@ defmodule Makeup.Lexers.SwiftLexer do
 
   @inline Application.compile_env(:makeup_html, :inline, false)
 
-  root_element_combinator = string("hello")
+  # Lines
+
+  # LF/U+000A
+  line_feed = ascii_char([?\n])
+  # CR/U+000D
+  carriage_return = ascii_char([?\r])
+
+  line_break =
+    choice([
+      line_feed,
+      carriage_return,
+      concat(line_feed, carriage_return)
+    ])
+
+  line = repeat(lookahead_not(line_break) |> utf8_string([], 1))
+
+  # Whitespace
+  whitespace_item = [?\r, ?\s, ?\n, ?\f]
+  whitespace = whitespace_item |> ascii_string(min: 1) |> token(:whitespace)
+
+  # Literals
+  #
+  # A literal is the source code representation of a value of a type, such as a number or string.
+  #
+  # The following are examples of literals:
+  #
+  # 42               // Integer literal
+  # 3.14159          // Floating-point literal
+  # "Hello, world!"  // String literal
+  # /Hello, .*/      // Regular expression literal
+  # true             // Boolean literal
+
+  ## String
+  quoted_text_item = utf8_string([], 1)
+
+  quoted_text = quoted_text_item
+
+  string_literal_opening_delimiter = string("\"")
+
+  string_literal_closing_delimiter = string("\"")
+
+  static_string_literal =
+    many_surrounded_by(
+      quoted_text,
+      string_literal_opening_delimiter,
+      string_literal_closing_delimiter
+    )
+
+  string_literal = static_string_literal |> token(:literal)
+
+  ## nil
+  nil_literal = string("nil") |> token(:literal)
+
+  ## Boolean
+  boolean_literal = choice([string("true"), string("false")]) |> token(:literal)
+
+  ## Numeric
+
+  ### Integer
+
+  #### Decimal
+  decimal_digit = ascii_string([?0..?9], min: 1)
+  decimal_literal = times(decimal_digit, min: 1)
+  integer_literal = decimal_literal |> token(:number_integer)
+
+  ### Float
+  sign = choice([string("+"), string("-")])
+  decimal_fraction = concat(string("."), decimal_literal)
+  floating_point_literal = concat(decimal_literal, decimal_fraction) |> token(:number_float)
+
+  ## All
+  numeric_literal =
+    choice([
+      floating_point_literal,
+      integer_literal
+    ])
+
+  ## All
+  literal =
+    choice([
+      nil_literal,
+      boolean_literal,
+      string_literal,
+      numeric_literal
+    ])
+
+  # Comments
+  comment_text = utf8_string([], 1)
+
+  ## Inline
+  inline_comment =
+    string("//")
+    |> concat(repeat(lookahead_not(line_break) |> utf8_string([], 1)))
+    |> token(:comment_single)
+
+  ## Multi-line
+  multiline_comment_text = comment_text
+
+  multiline_comment =
+    multiline_comment_text
+    |> many_surrounded_by("/*", "*/")
+    |> token(:comment_multiline)
+
+  # Root
+
+  root_element_combinator =
+    choice([
+      # Whitespace
+      whitespace,
+      # Literals
+      literal,
+      # Comments
+      inline_comment,
+      multiline_comment
+    ])
 
   @doc false
   def __as_swift_language__({ttype, meta, value}) do
--- test/makeup/lexers/swift_lexer_test.exs
+++ test/makeup/lexers/swift_lexer_test.exs
@@ -1,5 +1,98 @@
 defmodule Makeup.Lexers.SwiftLexerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
   alias Makeup.Lexers.SwiftLexer
+
+  describe "lex/1" do
+    test "inline comment" do
+      assert SwiftLexer.lex(~s(// Hello, World! program\n)) ==
+               [
+                 {
+                   :comment_single,
+                   %{language: :swift},
+                   [
+                     "//",
+                     " ",
+                     "H",
+                     "e",
+                     "l",
+                     "l",
+                     "o",
+                     ",",
+                     " ",
+                     "W",
+                     "o",
+                     "r",
+                     "l",
+                     "d",
+                     "!",
+                     " ",
+                     "p",
+                     "r",
+                     "o",
+                     "g",
+                     "r",
+                     "a",
+                     "m"
+                   ]
+                 },
+                 {:whitespace, %{language: :swift}, "\n"}
+               ]
+    end
+
+    test "multi-line comment" do
+      assert SwiftLexer.lex(~s(/* test */\n)) == [
+               {:comment_multiline, %{language: :swift},
+                [
+                  {:punctuation, %{}, "/*"},
+                  " ",
+                  "t",
+                  "e",
+                  "s",
+                  "t",
+                  " ",
+                  {:punctuation, %{}, "*/"}
+                ]},
+               {:whitespace, %{language: :swift}, "\n"}
+             ]
+    end
+
+    test "true boolean literal" do
+      assert SwiftLexer.lex(~s(true)) == [{:literal, %{language: :swift}, "true"}]
+    end
+
+    test "false boolean literal" do
+      assert SwiftLexer.lex(~s(false)) == [{:literal, %{language: :swift}, "false"}]
+    end
+
+    test "string literal" do
+      assert SwiftLexer.lex(~s("hello world")) == [
+               {
+                 :literal,
+                 %{language: :swift},
+                 ["\"", "h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "\""]
+               }
+             ]
+    end
+
+    test "nil literal" do
+      assert SwiftLexer.lex(~s(nil)) == [
+               {
+                 :literal,
+                 %{language: :swift},
+                 "nil"
+               }
+             ]
+    end
+
+    test "integer literal" do
+      assert SwiftLexer.lex(~s(59)) == [{:number_integer, %{language: :swift}, "59"}]
+    end
+
+    test "floating point literal" do
+      assert SwiftLexer.lex(~s(28.04)) == [
+               {:number_float, %{language: :swift}, ["28", ".", "04"]}
+             ]
+    end
+  end
 end
